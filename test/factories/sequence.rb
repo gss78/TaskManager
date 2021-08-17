304465a4843a6377bd68fae907fcbe723c67f42e
Sergey Glebov <4gss@mail.ru>
2021-08-17 11:04:33 +0400

    Improve factory by removing state and type fields generation

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,14 +1,12 @@
 FactoryBot.define do
-  sequence :string, aliases: [:state, :description, :name, :first_name, :last_name, :password, :avatar] do |n|
+  sequence :string, aliases: [:description, :name, :first_name, :last_name, :password, :avatar] do |n|
     "string#{n}"
   end
 
   sequence :email do |n|
     "person#{n}@example.com"
   end
-
-  sequence :type, ['Admin', 'Manager', 'Developer'].cycle
-
+  
   sequence :date, aliases: [:expired_at] do
     Date.today + rand(10..20)
   end
