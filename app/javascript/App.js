fd6a07a98941db0336b6a5d129b5183c3dacb934
Sergey Glebov <4gss@mail.ru>
2021-10-15 11:33:46 +0400

    Add MUITheme component

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -3,10 +3,13 @@ import React from 'react';
 import store from 'store';
 import { Provider } from 'react-redux';
 import TaskBoard from 'containers/TaskBoard';
+import MUITheme from 'MUITheme/MUITheme';
 
 const App = (user) => (
   <Provider store={store}>
-    <TaskBoard user={user} />
+    <MUITheme>
+      <TaskBoard user={user} />
+    </MUITheme>
   </Provider>
 );
 
