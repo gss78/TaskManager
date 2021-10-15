9d82f7ed4c29281b62018bf280b2dafa910762c9
Sergey Glebov <4gss@mail.ru>
2021-10-15 10:09:58 +0400

    Refactor loadColumnMore function 2

--- app/javascript/slices/TasksSlice.js
+++ app/javascript/slices/TasksSlice.js
@@ -2,7 +2,7 @@ import { propEq } from 'ramda';
 import { createSlice } from '@reduxjs/toolkit';
 import TasksRepository from 'repositories/TasksRepository';
 import { STATES } from 'presenters/TaskPresenter';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { changeColumn } from '@asseinfo/react-kanban';
 
 const initialState = {
@@ -31,16 +31,26 @@ const tasksSlice = createSlice({
 
       return state;
     },
+    loadColumnMoreSuccess(state, { payload }) {
+      const { items, meta, columnId } = payload;
+      const column = state.board.columns.find(propEq('id', columnId));
+
+      state.board = changeColumn(state.board, column, {
+        cards: [...column.cards, ...items],
+        meta,
+      });
+
+      return state;
+    },
   },
 });
 
-const { loadColumnSuccess } = tasksSlice.actions;
+const { loadColumnSuccess, loadColumnMoreSuccess } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
 
 export const useTasksActions = () => {
   const dispatch = useDispatch();
-  const board = useSelector((state) => state.TasksSlice.board);
 
   const loadColumn = (state, page = 1, perPage = 10) => {
     TasksRepository.index({
@@ -58,11 +68,7 @@ export const useTasksActions = () => {
       page,
       perPage,
     }).then(({ data }) => {
-      const cards = { items: [...data.items], meta: { ...data.meta } };
-      const column = board.columns.find(propEq('id', state));
-
-      cards.items = [...column.cards, ...data.items];
-      dispatch(loadColumnSuccess({ ...cards, columnId: state }));
+      dispatch(loadColumnMoreSuccess({ ...data, columnId: state }));
     });
   };
 
