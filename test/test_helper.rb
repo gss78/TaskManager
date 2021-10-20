28e03f7318dec1fd913f8f84a557ff85af94b7e1
Sergey Glebov <4gss@mail.ru>
2021-10-20 10:13:36 +0400

    Install coveralls gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require 'simplecov'
 SimpleCov.start
+require 'coveralls'
+Coveralls.wear!('rails')
 require_relative '../config/environment'
 require 'rails/test_help'
 
