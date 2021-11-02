f32f168ba3b89819efcab6bfc8c7d5285f2990fa
Sergey Glebov <4gss@mail.ru>
2021-11-02 16:27:52 +0400

    Add action_mailer default url options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
     Bullet.bullet_logger = true
   end
 
+  host = 'vbox.local:3000' # Local server 
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_caching = true
 
