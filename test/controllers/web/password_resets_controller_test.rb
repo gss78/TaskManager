a36b6a3095171074c58889073389d264e8cb7a74
Sergey Glebov <4gss@mail.ru>
2021-11-04 13:32:36 +0400

    Implement PasswordResetsController tests

--- test/controllers/web/password_resets_controller_test.rb
+++ test/controllers/web/password_resets_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class Web::PasswordResetsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+  end
+
+  test 'should post create' do
+    user = create(:user)
+    attrs = {
+      email: user.email,
+    }
+    post :create, params: { password_reset: attrs }
+    assert_not flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should patch update' do
+    user = create(:user)
+    user.create_reset_digest
+    attrs = {
+      use_route: 'password_resets',
+      id: user.reset_token,
+      email: user.email,
+      user: 
+        { 
+          password: '123',
+          password_confirmation: '123'
+        }
+    }
+    patch :update, params: attrs
+    assert_redirected_to root_url
+    assert_not flash.empty?
+  end
+
 end
