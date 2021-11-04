83459cc3371fcc74bd576dd18aa07fadacdd2fea
Sergey Glebov <4gss@mail.ru>
2021-11-04 13:42:27 +0400

    Implement password reset related methods in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i.freeze
   validates :email, format: { with: VALID_EMAIL_REGEX }
   validates :email, uniqueness: true
+  attr_accessor :reset_token
+
+  PASSWORD_RESET_TOKEN_EXPIRATION_IN_HOURS = 24
 
   def admin?
     is_a?(Admin)
@@ -27,4 +30,46 @@ class User < ApplicationRecord
   def author_of?(task)
     task.try(:author) == self
   end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Returns a random token.
+  def User.new_token 
+    SecureRandom.urlsafe_base64
+  end
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email 
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Returns true if a password reset has expired. 
+  def password_reset_expired?
+    reset_sent_at < PASSWORD_RESET_TOKEN_EXPIRATION_IN_HOURS.hours.ago
+  end
+
+  # Returnss true if user authenticated by token
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?   
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def clear_reset_data
+    update_attribute(:reset_digest, nil)
+    update_attribute(:reset_sent_at, nil)
+  end
+
+    
 end
