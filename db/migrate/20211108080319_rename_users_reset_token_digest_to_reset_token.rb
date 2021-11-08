950c46baa0232afb39bb78017bee3fba266f9b27
Sergey Glebov <4gss@mail.ru>
2021-11-08 15:26:52 +0400

    Update migrate file

--- db/migrate/20211108080319_rename_users_reset_token_digest_to_reset_token.rb
+++ db/migrate/20211108080319_rename_users_reset_token_digest_to_reset_token.rb
@@ -1,5 +1,5 @@
 class RenameUsersResetTokenDigestToResetToken < ActiveRecord::Migration[6.1]
   def change
-    rename_column :users, :reset_token_digest, :reset_token
+    rename_column :users, :reset_digest, :reset_token
   end
 end
