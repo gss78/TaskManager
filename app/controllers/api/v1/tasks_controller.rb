c4b5fe6d65afff5b8f58d1d121441decc10a5764
Sergey Glebov <4gss@mail.ru>
2021-11-09 14:40:26 +0400

    Dispatching emails in background

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -27,7 +27,7 @@ class Api::V1::TasksController < Api::V1::ApplicationController
            end
 
     if task.save 
-      UserMailer.with({ user: task.author, task: task }).task_created.deliver_now
+      UserMailer.with({ user: task.author, task: task }).task_created.deliver_later
     end  
 
     respond_with(task, serializer: TaskSerializer, location: nil)
@@ -39,7 +39,7 @@ class Api::V1::TasksController < Api::V1::ApplicationController
     attrs = permitted_attributes(task)
 
     if task.update(validate(attrs))
-      UserMailer.with({ user: task.author, task: task }).task_updated.deliver_now
+      UserMailer.with({ user: task.author, task: task }).task_updated.deliver_later
     end
 
     respond_with(task, serializer: TaskSerializer)
@@ -50,7 +50,7 @@ class Api::V1::TasksController < Api::V1::ApplicationController
     authorize(task)
 
     if task.destroy
-      UserMailer.with({ user: task.author, task: task }).task_deleted.deliver_now
+      UserMailer.with({ user: task.author, task: task }).task_deleted.deliver_later
     end
 
     respond_with(task)
